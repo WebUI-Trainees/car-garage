ac165915d795aaff11ed3efa767b57b3fa07bae3
Pavlina <30826013+Pavlina95@users.noreply.github.com>
2017-12-05 22:19:44 +0200

    Added the routes for the different single-model-view. (#10)

--- src/components/modelsComponents/ModelListItem.jsx
+++ src/components/modelsComponents/ModelListItem.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const ModelListItem = ({ model }) => (
-  <div className="model-container text-center">
-    <h3 className="model text-left">{model.name}</h3>
-    <h3 className="model text-left">{model.price}</h3>
-    <div className="car-image">
-      <img src={model.image} alt={model.model} />
-    </div>
-    <div className="buttons-container">
-      <a className="d-inline-block">
-        <div className="button-with-icon d-inline-block">
-          <div className="icon">
-            <div className="icon-image" />
+const ModelListItem = ({ model }) => {
+  const path = () => `/model/${model.id}`;
+
+  return (
+    <div className="model-container text-center">
+      <h3 className="model text-left">{model.name}</h3>
+      <h3 className="model text-left">{model.price}</h3>
+      <div className="car-image">
+        <img src={model.image} alt={model.model} />
+      </div>
+      <div className="buttons-container">
+        <div className="d-inline-block">
+          <div className="button-with-icon d-inline-block">
+            <div className="icon">
+              <div className="icon-image" />
+            </div>
+            <Link to={path()}>
+              <p className="button-text">View more</p>
+            </Link>
           </div>
-          <p className="button-text">View more</p>
         </div>
-      </a>
-      <a className="d-inline-block">
-        <div className="square-button">
-          <div className="icon configure" />
-        </div>
-      </a>
-      <a className="d-inline-block">
-        <div className="square-button">
-          <div className="icon buy" />
-        </div>
-      </a>
+        <a className="d-inline-block">
+          <div className="square-button">
+            <div className="icon configure" />
+          </div>
+        </a>
+        <a className="d-inline-block">
+          <div className="square-button">
+            <div className="icon buy" />
+          </div>
+        </a>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 ModelListItem.propTypes = {
   model: PropTypes.shape({
