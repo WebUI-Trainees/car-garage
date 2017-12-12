58aae2c045bfbd913145f7838717290a0ab3b165
Stoil Filiov <stoil_e@abv.bg>
2017-12-12 15:36:16 +0200

    Test drive style update (#17)
    * updating testdrive page
    
    * style update for test drive page

--- src/components/testDriveComponents/Store.jsx
+++ src/components/testDriveComponents/Store.jsx
@@ -4,6 +4,10 @@ const Store = () => (
   <div className="store">
     <div className="orders">
       <div>
+        <img alt="" src="/src/images/driveAssets/information.png" width="300" />
+        <p>
+          This is the test drive SSBenz page. If you want to test one of our Mercedes-Benz models you can use the chat
+          to register for test drive. For more information type `help` in chatbox.
         <p>
           This is the test drive SSBenz page. Use the chat to register for test drive. For more info type `help` in
           chatbox.
