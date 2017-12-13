a383fac6b91925c88df95d359e506d27eb182e7b
Stoil Filiov <stoil_e@abv.bg>
2017-12-13 14:07:00 +0200

    Update footer icons (#22)
    * updating footer
    
    * create footer icons

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,19 +2,47 @@ import React from 'react';
 import KeepConnectedItem from './KeepConnectedItem';
 
 const socialLinks = [
-  <h3>Keep Connected</h3>,
-  <span className="fa fa-facebook" />,
-  <span className="fa fa-twitter" />,
-  <span className="fa fa-google" />,
-  <span className="fa fa-linkedin" />
+  <div>
+    <h3>Keep Connected</h3>
+    <ul>
+      <li>
+        <span className="fa fa-facebook" />
+        <span>Like us on Facebook</span>
+      </li>
+      <li>
+        <span className="fa fa-twitter" />
+        <span>Follow us on Twitter</span>
+      </li>
+      <li>
+        <span className="fa fa-google" />
+        <span>Add us on Google+</span>
+      </li>
+      <li>
+        <span className="fa fa-linkedin" />
+        <span>Follow us on LinkedIn</span>
+      </li>
+    </ul>
+  </div>
 ];
 
 const contactLinks = [
-  <h3>Contact Information</h3>,
-  <span className="fa fa-envelope" />,
-  <span className="fa fa-map-marker" />,
-  <span className="fa fa-phone" />
-];
+  <div>
+    <h3>Contact Information</h3>
+    <ul>
+      <li>
+        <span className="fa fa-envelope" />
+        <span>ssbenz@mercedes.com</span>
+      </li>
+      <li>
+        <span className="fa fa-map-marker" />
+        <span>Blvd. Vitosha 777, Sofia</span>
+      </li>
+      <li>
+        <span className="fa fa-phone" />
+        <span>+359 777 777 777</span>
+      </li>
+    </ul>
+  </div>
 
 const Footer = () => (
   <footer>
