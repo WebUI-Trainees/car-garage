74c7f9a60bbb987f09e7b0fd25be590805989860
Stoil Filiov <sfiliotc@training.local>
2017-12-14 18:00:26 +0200

    update the footer and header

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,41 +3,37 @@ import KeepConnectedItem from './KeepConnectedItem';
 
 const socialLinks = [
   <div>
-    <h3>Keep Connected</h3>
-    <ul>
-      <li>
-        <span className="fa fa-facebook" />
-        <span>Like us on Facebook</span>
-      </li>
-      <li>
-        <span className="fa fa-twitter" />
-        <span>Follow us on Twitter</span>
-      </li>
-      <li>
-        <span className="fa fa-google" />
-        <span>Add us on Google+</span>
-      </li>
-    </ul>
+    <h3 id="footer-heading">Keep Connected</h3>
+    <span className="fa fa-facebook" />
+    <span>Like us on Facebook</span>
+    <span className="fa fa-twitter" />
+    <span>Follow us on Twitter</span>
+    <span className="fa fa-google" />
+    <span>Add us on Google+</span>
   </div>
 ];
 
 const contactLinks = [
   <div>
-    <h3>Contact Information</h3>
-    <ul>
-      <li>
-        <span className="fa fa-envelope" />
-        <span>ssbenz@mercedes.com</span>
-      </li>
-      <li>
-        <span className="fa fa-map-marker" />
-        <span>Blvd. Vitosha 777, Sofia</span>
-      </li>
-      <li>
-        <span className="fa fa-phone" />
-        <span>+359 777 777 777</span>
-      </li>
-    </ul>
+    <h3 id="footer-heading">Contact Information</h3>
+    <span className="fa fa-envelope" />
+    <span>ssbenz@mercedes.com</span>
+    <span className="fa fa-map-marker" />
+    <span>Blvd. Vitosha 777, Sofia</span>
+    <span className="fa fa-phone" />
+    <span>+359 777 777 777</span>
+  </div>
+];
+
+const services = [
+  <div>
+    <h3 id="footer-heading">Services</h3>
+    <span className="fa fa-car" />
+    <span>Rent a Car</span>
+    <span className="fa fa-credit-card" />
+    <span>Leasing options</span>
+    <span className="fa fa-shield" />
+    <span>Insurance</span>
   </div>
 ];
 
@@ -45,7 +41,7 @@ const Footer = () => (
   <footer>
     <KeepConnected />
     <ContactInfo />
-    <Map />
+    <Services />
   </footer>
 );
 
@@ -53,14 +49,6 @@ const KeepConnected = () => <div>{socialLinks.map(link => <KeepConnectedItem key
 
 const ContactInfo = () => <div>{contactLinks.map(link => <KeepConnectedItem key={link} title={link} />)}</div>;
 
-const Map = () => (
-  <div>
-    <select>
-      <option>All</option>
-      <option>Mechanics</option>
-      <option>Dealers</option>
-    </select>
-  </div>
-);
+const Services = () => <div>{services.map(link => <KeepConnectedItem key={link} title={link} />)}</div>;
 
 export default Footer;
